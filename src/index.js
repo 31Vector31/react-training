3c19d5e99eecede16e0d0989be5f235b5545ad80
31Vector31 <Y..u..r..a..2002>
2021-11-02 17:36:35 +0200

    added reviews

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ContactsList from './ContactsList/ContactsList';
+import Reviews from './Reviews/Reviews';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ContactsList />
+    <Reviews />
   </React.StrictMode>,
   document.getElementById('root')
 );
