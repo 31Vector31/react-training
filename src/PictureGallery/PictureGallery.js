76f5a6b8936837a50a6cfac343257790061b4b05
Yura <lutaiyura@gmail.com>
2021-12-08 18:28:13 +0200

    fix picture gallery

--- src/PictureGallery/PictureGallery.js
+++ src/PictureGallery/PictureGallery.js
@@ -1,14 +1,25 @@
-import React, {useState, useCallback, useEffect} from 'react';
+import React, {useState, useCallback, useEffect, useRef} from 'react';
 import Pictures from './Pictures';
 import Search from './Search';
 import {getPictures} from './ApiRequests';
+
+function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+        ref.current = value;
+    }, [value]);
+    return ref.current;
+}
 
 function PictureGallery() {
     const [pictures, setPictures] = useState([]);
     const [search, setSearch] = useState(null);
     const [page, setPage] = useState(1);
 
+    const prevSearch = usePrevious(search);
+
     useEffect(() => {
+        if (search === prevSearch && page === 1) return;
         getPictures(search, page).then(pictures => {
             setPictures((prevPictures) => [...prevPictures, ...pictures]);
         });
